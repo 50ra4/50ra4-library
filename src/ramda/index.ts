1c95d433a59c6f791bcb8f607a05b17313957aba
50ra4 <50ra4@MacBook-Pro.local>
2020-10-13 20:06:27 +0900

    add: initial setting

---
+++ src/ramda/index.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-restricted-imports */
+import pipe from 'ramda/src/pipe';
+import pick from 'ramda/src/pick';
+import omit from 'ramda/src/omit';
+import prop from 'ramda/src/prop';
+import path from 'ramda/src/path';
+
+import partial from 'ramda/src/partial';
+import partialRight from 'ramda/src/partialRight';
+import drop from 'ramda/src/drop';
+import head from 'ramda/src/head';
+import map from 'ramda/src/map';
+
+import equals from 'ramda/src/equals';
+import propSatisfies from 'ramda/src/propSatisfies';
+import uniqBy from 'ramda/src/uniqBy';
+import pathOr from 'ramda/src/pathOr';
+import sortBy from 'ramda/src/sortBy';
+
+import eqProps from 'ramda/src/eqProps';
+import not from 'ramda/src/not';
+import times from 'ramda/src/times';
+import repeat from 'ramda/src/repeat';
+
+import fromPairs from 'ramda/src/fromPairs';
+import toPairs from 'ramda/src/toPairs';
+
+export {
+  pipe,
+  pick,
+  path,
+  omit,
+  partial,
+  partialRight,
+  drop,
+  head,
+  map,
+  equals,
+  propSatisfies,
+  uniqBy,
+  pathOr,
+  sortBy,
+  prop,
+  eqProps,
+  not,
+  times,
+  repeat,
+  fromPairs,
+  toPairs,
+};
