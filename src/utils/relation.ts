a13381423de05d1e3e814bb71b7ef34a421a92d0
50ra4 <50ra4@MacBook-Pro.local>
2020-10-28 23:34:14 +0900

    fix: relation function

--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -3,34 +3,34 @@ export function lt(a: number, b: number): boolean;
 export function lt(a: number): (b: number) => boolean;
 export function lt(a: number, b?: number) {
   if (arguments.length === 1) {
-    return (_b: number) => a < _b;
+    return (_b: number) => _b < a;
   }
-  return a < b!;
+  return b! < a;
 }
 
 export function lte(a: number, b: number): boolean;
 export function lte(a: number): (b: number) => boolean;
 export function lte(a: number, b?: number) {
   if (arguments.length === 1) {
-    return (_b: number) => a <= _b;
+    return (_b: number) => _b <= a;
   }
-  return a <= b!;
+  return b! <= a;
 }
 
 export function gt(a: number, b: number): boolean;
 export function gt(a: number): (b: number) => boolean;
 export function gt(a: number, b?: number) {
   if (arguments.length === 1) {
-    return (_b: number) => a > _b;
+    return (_b: number) => _b > a;
   }
-  return a > b!;
+  return b! > a;
 }
 
 export function gte(a: number, b: number): boolean;
 export function gte(a: number): (b: number) => boolean;
 export function gte(a: number, b?: number) {
   if (arguments.length === 1) {
-    return (_b: number) => a >= _b;
+    return (_b: number) => _b >= a;
   }
-  return a >= b!;
+  return b! >= a;
 }
