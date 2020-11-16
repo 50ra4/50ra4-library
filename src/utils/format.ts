1fbe757e94a355d29f8a52845dc100753b4d65af
50ra4 <50ra4@MacBook-Pro.local>
2020-11-16 20:17:47 +0900

    add: toString

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,53 @@
 import { LogLevel } from '.';
-import { format } from 'date-fns/fp';
-import { toPairs, toString } from '../ramda';
+import format from 'date-fns/format';
+import { toPairs } from '../ramda';
 import { EDateFormat } from './date';
+import {
+  isArray,
+  isBigint,
+  isBoolean,
+  isDate,
+  isFunction,
+  isNull,
+  isNumber,
+  isObject,
+  isRegExp,
+  isString,
+  isSymbol,
+  isUndefined,
+} from './typeGuard';
+
+// eslint-disable-next-line complexity
+export const toString = <T = unknown>(value: T): string => {
+  if (isString(value) || isNumber(value) || isSymbol(value) || isBigint(value)) {
+    return value.toString();
+  }
+  if (isArray(value)) {
+    return `[${value.map(toString)}]`;
+  }
+  if (isDate(value)) {
+    return value.toISOString();
+  }
+  if (isObject(value)) {
+    return JSON.stringify(value);
+  }
+  if (isBoolean(value)) {
+    return value ? 'true' : 'false';
+  }
+  if (isUndefined(value)) {
+    return 'undefined';
+  }
+  if (isNull(value)) {
+    return 'null';
+  }
+  if (isRegExp(value)) {
+    return 'regExp';
+  }
+  if (isFunction(value)) {
+    return `function(${value.name})`;
+  }
+  return 'unknown';
+};
 
 export const replaceMessage = (template: string, replaceParams: Record<string | number, unknown>): string =>
   toPairs(replaceParams).reduce(
@@ -9,7 +55,7 @@ export const replaceMessage = (template: string, replaceParams: Record<string | 
     template,
   );
 
-const getTimeStamp = () => format(EDateFormat.timestampISO, new Date());
+const getTimeStamp = () => format(new Date(), EDateFormat.timestampISO);
 
 export const formatLogMessage = (logLevel: LogLevel, message: string): string =>
   `[${logLevel}] ${getTimeStamp()}: ${message}`;
