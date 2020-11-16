4b0b3f7504c4462b407d2f97d3ec456463dd4383
50ra4 <50ra4@MacBook-Pro.local>
2020-11-16 20:16:44 +0900

    fix: common types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
-export type ObjectType = Record<string, unknown>;
-export type IndexedObject<T> = Record<string, T>;
+export type ObjectKeyType = string | number | symbol;
+export type ObjectType<T extends ObjectKeyType = string, K = unknown> = Record<T, K>;
+export type IndexedObject<T> = ObjectType<string, T>;
 export type Nullable<T> = T | undefined | null;
 export type ReadonlyPartial<T> = Readonly<Partial<T>>;
-export type SafePred<A extends ReadonlyArray<unknown>> = (...a: A) => boolean;
+export type FunctionType<A extends ReadonlyArray<unknown>, B = unknown> = (...a: A) => B;
+export type SafePred<A extends ReadonlyArray<unknown>> = FunctionType<A, boolean>;
