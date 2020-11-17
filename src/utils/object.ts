1598e9a8a22d6fbc7f8660c70b760841bd5bc80b
sigarashi <48591598+shigarashi1@users.noreply.github.com>
2020-11-17 21:42:41 +0900

    fix: objectUtils (#24)
    Co-authored-by: 50ra4 <50ra4@MacBook-Pro.local>

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,4 +1,4 @@
-import { ObjectType, ObjectKeyType } from '../types';
+import { ObjectType } from '../types';
 
 export const toPairs = <V extends ObjectType>(obj: V): ReadonlyArray<[keyof V, V[keyof V]]> => {
   return Object.entries(obj) as [keyof V, V[keyof V]][];
@@ -8,16 +8,16 @@ export const keys = <V extends ObjectType>(obj: V): ReadonlyArray<keyof V> => {
   return Object.keys(obj) as (keyof V)[];
 };
 
-export const values = <V extends ObjectType, K extends keyof V>(obj: V): ReadonlyArray<V[K]> => {
-  return Object.values(obj) as V[K][];
+export const values = <T extends ObjectType, K extends keyof T>(obj: T): ReadonlyArray<T[K]> => {
+  return Object.values(obj) as T[K][];
 };
 
-export const pick = <K extends ObjectKeyType, T extends ObjectType<K>>(keys: K[], obj: T): Readonly<Pick<T, K>> => {
+export const pick = <T extends ObjectType, K extends keyof T>(keys: K[], obj: T): Readonly<Pick<T, K>> => {
   return Object.fromEntries(toPairs<T>(obj).filter(([key]) => keys.includes(key as K))) as Pick<T, K>;
 };
 
-export const omit = <K extends ObjectKeyType, T extends ObjectType<K>>(keys: K[], obj: T): Readonly<Omit<T, K>> => {
+export const omit = <T extends ObjectType, K extends keyof T>(keys: K[], obj: T): Readonly<Omit<T, K>> => {
   return (Object.fromEntries(toPairs<T>(obj).filter(([key]) => !keys.includes(key as K))) as unknown) as Omit<T, K>;
 };
 
-export const prop = <K extends ObjectKeyType, T extends ObjectType<K>>(key: K, obj: T): Readonly<T[K]> => obj[key];
+export const prop = <T extends ObjectType, K extends keyof T>(key: K, obj: T): Readonly<T[K]> => obj[key];
